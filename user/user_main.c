aab40bd889a2c7a54949dbca9e8cca552e2d816c
candale <candale.andrei@gmail.com>
2017-02-14 21:11:03 +0200

    Updated for new reading procedure

--- user/user_main.c
+++ user/user_main.c
@@ -14,7 +14,9 @@
 
 #define DEEP_SLEEP_OPERATION 1
 #define NORMAL_OPERATION     2
-#define THERMOSTAT_OPERATION_MODE NORMAL_OPERATION
+#define OPERATION_MODE DEEP_SLEEP_OPERATION
+// In seconds
+#define SLEEP_TIME 300
 
 #define ABS(x) (x < 0 ? x * -1 : x)
 
@@ -45,14 +47,14 @@ go_to_sleep(void *arg) {
     os_timer_disarm(&sleep_timer);
     // 0 means Radio calibration after deep-sleep wake up depends on init data byte 108.
     system_deep_sleep_set_option(0);
-    system_deep_sleep(1200 * 1000 * 1000);
+    system_deep_sleep(SLEEP_TIME * 1000 * 1000);
 }
 
 void ICACHE_FLASH_ATTR
 read_and_publish_data() {
     char* temp_str = (char*)os_zalloc(32);
 
-
+    ds18b20_request_temperatures(&sensors);
     if(sensors.count >= 1) {
         INFO("Reading temperature 1 ...\n");
         float temperature1 = ds18b20_read(&sensors, 0);
@@ -60,7 +62,6 @@ read_and_publish_data() {
         INFO("Publishing TEMP 1: %s\n", temp_str);
         MQTTRPC_Publish(&rpc_conf, "temperature", temp_str, os_strlen(temp_str), 2, 1);
     }
-
 
     if(sensors.count >= 2) {
         INFO("Reading temperature 2 ...\n");
@@ -82,17 +83,17 @@ read_and_publish_data_fn(void* arg) {
 
 void ICACHE_FLASH_ATTR
 setup_worker(void* arg) {
-    if(THERMOSTAT_OPERATION_MODE == DEEP_SLEEP_OPERATION) {
+    if(OPERATION_MODE == DEEP_SLEEP_OPERATION) {
         read_and_publish_data();
 
         // Give it some time so MQTT can do its thing and then go to sleep
         os_timer_disarm(&sleep_timer);
         os_timer_setfn(&sleep_timer, (os_timer_func_t*)go_to_sleep, (void *)0);
         os_timer_arm(&sleep_timer, 2000, 0);
-    } else if(THERMOSTAT_OPERATION_MODE == NORMAL_OPERATION) {
+    } else if(OPERATION_MODE == NORMAL_OPERATION) {
         os_timer_disarm(&read_and_pub_timer);
         os_timer_setfn(&read_and_pub_timer, (os_timer_func_t*)read_and_publish_data_fn, (void*)0);
-        os_timer_arm(&read_and_pub_timer, 3000, 1);
+        os_timer_arm(&read_and_pub_timer, 7000, 1);
     }
 }
 
@@ -129,7 +130,7 @@ void ICACHE_FLASH_ATTR
 init_all(void) {
     // system_set_os_print(0);
     uart_div_modify(0, UART_CLK_FREQ / 115200);
-    print_info();
+    // print_info();
 
     INFO("Connecting to wifi...\n");
     WIFI_Connect(WIFI_SSID, WIFI_PASS, init_mqtt_rpc);
